17be927e31f7484c642b1d930c0c192c0fc5e6d0
bianchui <bianchui@gmail.com>
2018-06-25 10:12:01 +0800

    libs:glfw-3.2.1

--- xxprofile/src/xxprofile.cpp
+++ xxprofile/src/xxprofile.cpp
@@ -15,8 +15,7 @@
 XX_NAMESPACE_BEGIN(xxprofile);
 
 static pthread_key_t g_profile_tls_key;
-static pthread_once_t g_profile_init_once = PTHREAD_ONCE_INIT;
-static __thread XXProfileTLS* g_tls_profile;
+//static pthread_once_t g_profile_init_once = PTHREAD_ONCE_INIT;
 
 // XXProfile
 static void profile_on_thread_exit(void* data) {
@@ -24,32 +23,40 @@ static void profile_on_thread_exit(void* data) {
     delete profile;
 }
 
-static void profile_tls_init_once() {
-    pthread_key_create(&g_profile_tls_key, profile_on_thread_exit);
-    XXProfileTimer::InitTiming();
-}
+//static void profile_tls_init_once() {
+//}
 
 bool XXProfile::StaticInit() {
-    pthread_once(&g_profile_init_once, profile_tls_init_once);
+    //pthread_once(&g_profile_init_once, profile_tls_init_once);
+    if (!g_profile_tls_key) {
+        pthread_key_create(&g_profile_tls_key, profile_on_thread_exit);
+    }
+    XXProfileTimer::InitTiming();
     return true;
 }
 
 void XXProfile::StaticUninit() {
+    if (g_profile_tls_key) {
+        XXProfileTLS* profile = (XXProfileTLS*)pthread_getspecific(g_profile_tls_key);
+        if (profile) {
+            delete profile;
+            pthread_setspecific(g_profile_tls_key, NULL);
+        }
+    }
     pthread_key_delete(g_profile_tls_key);
     g_profile_tls_key = 0;
-    if (g_tls_profile) {
-        delete g_tls_profile;
-        g_tls_profile = NULL;
-    }
 }
 
 XXProfileTLS* XXProfileTLS::Get() {
-    if (!g_tls_profile && g_profile_tls_key) {
-        XXProfileTLS* profile = new XXProfileTLS();
-        pthread_setspecific(g_profile_tls_key, profile);
-        g_tls_profile = profile;
+    XXProfileTLS* profile = NULL;
+    if (g_profile_tls_key) {
+        profile = (XXProfileTLS*)pthread_getspecific(g_profile_tls_key);
+        if (!profile) {
+            profile = new XXProfileTLS();
+            pthread_setspecific(g_profile_tls_key, profile);
+        }
     }
-    return g_tls_profile;
+    return profile;
 }
 
 bool XXProfile::IncreaseFrame() {
