50cb165681c113f27ce4712aa31a77c2ac5d571a
bianchui <bianchui@gmail.com>
2019-02-13 10:47:07 +0800

    shared: macros: CLASS_DELETE_COPY_MOVE

--- shared/SharedMacros.h
+++ shared/SharedMacros.h
@@ -1,6 +1,6 @@
 // Copyright 2018 bianchui. All rights reserved.
-#ifndef shared_SharedMacros_h__
-#define shared_SharedMacros_h__
+#ifndef __shared_SharedMacros_h__
+#define __shared_SharedMacros_h__
 
 #define CLASS_DELETE_COPY_CONSTRUCTOR(cls) \
 /**/cls(const cls&) = delete; \
@@ -22,6 +22,25 @@
 /**/cls(const cls&&) = delete; \
 /**/cls& operator=(const cls&&) = delete; \
 
+#define CLASS_DELETE_COPY_MOVE(cls) \
+/**/CLASS_DELETE_COPY(cls); \
+/**/CLASS_DELETE_MOVE(cls); \
+
 #define ARRAY_COUNTOF(c) (sizeof(c) / sizeof(c[0]))
 
-#endif//shared_SharedMacros_h__
+#ifdef NDEBUG
+
+#  define DEBUGF(...)
+#  define DEBUG_ONLY(...)
+
+#else
+
+#  define DEBUGF(...) printf(__VA_ARGS__)
+#  define DEBUG_ONLY(...) __VA_ARGS__
+
+#endif//DEBUG
+
+#define NAMESPACE_BEGIN(ns) namespace ns {
+#define NAMESPACE_END(ns) }
+
+#endif//__shared_SharedMacros_h__
