b221392612f3d16ee4449f7756a7e648ffb50bbd
bianchui <bianchui@gmail.com>
2019-04-11 15:25:43 +0800

    xxprofile: android x86_64

--- xxprofile/proj.android/jni/Application.mk
+++ xxprofile/proj.android/jni/Application.mk
@@ -7,4 +7,4 @@ APP_CPPFLAGS += -fvisibility-inlines-hidden
 APP_CPPFLAGS += -D_GLIBCXX_HAVE_ATTRIBUTE_VISIBILITY=0
 APP_PLATFORM := android-19
 NDK_TOOLCHAIN_VERSION := clang
-APP_ABI := armeabi armeabi-v7a x86 arm64-v8a
+APP_ABI := armeabi armeabi-v7a x86 arm64-v8a x86_64
