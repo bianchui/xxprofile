fb87737157e2362fd5e616fb55ed206def92c74a
bianchui <bianchui@gmail.com>
2020-07-06 12:44:27 +0800

    shared: TLS:mac

--- shared/platforms/macos/Platform_macos.mm
+++ shared/platforms/macos/Platform_macos.mm
@@ -23,4 +23,25 @@ void platformSetThreadName(const char* name) {
     pthread_setname_np(name);
 }
 
+#pragma mark - TLS
+
+TLSKey platformTLSCreate(TLSValueDestructor des) {
+    static_assert(sizeof(pthread_key_t) == sizeof(TLSKey), "TLSKeySize");
+    pthread_key_t thread_key;
+    pthread_key_create(&thread_key, des);
+    return (TLSKey)thread_key;
+}
+
+void* platformTLSGet(TLSKey key) {
+    return pthread_getspecific((pthread_key_t)key);
+}
+
+bool platformTLSSet(TLSKey key, void* value) {
+    return 0 == pthread_setspecific((pthread_key_t)key, value);
+}
+
+void platformTLSDestroy(TLSKey key) {
+    pthread_key_delete((pthread_key_t)key);
+}
+
 SHARED_NAMESPACE_END;
