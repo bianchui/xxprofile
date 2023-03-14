9d6f37c3dea90e6188a7a4fd1490d222b8fbf7ef
bianchui <bianchui@gmail.com>
2023-03-14 18:57:03 +0800

    xxprofile: test_compress

---
+++ xxprofile/test_compress/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test_compress
+//
+//  Created by bianchui on 2023/3/14.
+//  Copyright © 2023 bianchui. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
