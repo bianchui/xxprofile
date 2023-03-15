d54d125ce0f6029681524fd4b6e45aec818c25da
bianchui <bianchui@gmail.com>
2023-03-15 19:12:51 +0800

    version: 1.2023.0315 with zstd

--- xxprofile/proj.android/jni/xxprofile_lib.cpp
+++ xxprofile/proj.android/jni/xxprofile_lib.cpp
@@ -9,9 +9,15 @@
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
 
-#include "../../../libs/lz4-1.9.2/lib/lz4.c"
+#if XX_IsCompress(LZ4) || XX_IsCompress(LZ4_CHUNKED)
+#include "../../../libs/lz4-1.9.4/lib/lz4.c"
 #ifdef XX_LZ4_USE_HC
-#include "../../../libs/lz4-1.9.2/lib/lz4hc.c"
-#endif//XX_LZ4_USE_HC
+#include "../../../libs/lz4-1.9.4/lib/lz4hc.c"
+#endif // XX_LZ4_USE_HC
+#endif // XX_IsCompress(LZ4) || XX_IsCompress(LZ4_CHUNKED)
+
+#if XX_IsCompress(ZSTD) || XX_IsCompress(ZSTD_CHUNKED)
+#include "../../src/compress/compress_zstd.lib.c"
+#endif // XX_IsCompress(ZSTD) || XX_IsCompress(ZSTD_CHUNKED)
 
 #pragma clang diagnostic pop
