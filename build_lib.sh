fc00ab7a8cf513949df938d8291ecf14ffd5ad63
bianchui <bianchui@gmail.com>
2023-03-16 12:27:17 +0800

    build: fix prebuilt Android.mk

--- build_lib.sh
+++ build_lib.sh
@@ -8,7 +8,7 @@ popd
 
 pushd $THIS_DIR/xxprofile/proj.android
   ./build.sh
-  cp ./prebuilt_Android.mk $THIS_DIR/out/android/Android.mk
+  cp ./prebuilt_Android.mk $THIS_DIR/out/prebuilt/android/Android.mk
 popd
 
 mkdir -p $THIS_DIR/out/include
