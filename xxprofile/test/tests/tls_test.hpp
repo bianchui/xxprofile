0437b3528d26f6273bb48687d7b3a35aafa9f6ab
bianchui <bianchui@gmail.com>
2023-07-04 15:45:06 +0800

    xxprofile: fix tls_test

--- xxprofile/test/tests/tls_test.hpp
+++ xxprofile/test/tests/tls_test.hpp
@@ -4,6 +4,7 @@
 
 #include <pthread.h>
 #include <thread>
+#include <chrono>
 
 namespace test {
 
@@ -19,14 +20,13 @@ static volatile int g_add = 1;
 static pthread_key_t g_key;
 
 static void test_tls_thread_local_thread() {
-  struct timespec start;
-  struct timespec end;
-  clock_gettime(CLOCK_MONOTONIC, &start);
+  auto start = std::chrono::steady_clock::now();
   for (int i = 0; i < 1000000; ++i) {
     g_test_tls_test += g_add;
   }
-  clock_gettime(CLOCK_MONOTONIC, &end);
-  printf("tls_thread_local: %d\n", (int)shared::timespec_sub_us(end, start));
+  auto end = std::chrono::steady_clock::now();
+  std::chrono::duration<double> diff = end - start;
+  printf("tls_thread_local: %fs\n", diff.count());
   printf("g_test_tls_test: %d\n", g_test_tls_test);
 }
 
@@ -41,15 +41,14 @@ static void test_tls_pthread() {
   pthread_key_create(&g_key, tls_pthread_callback);
   int a = 0;
   pthread_setspecific(g_key, &a);
-  struct timespec start;
-  struct timespec end;
-  clock_gettime(CLOCK_MONOTONIC, &start);
+  auto start = std::chrono::steady_clock::now();
   for (int i = 0; i < 1000000; ++i) {
     int* p = (int*)pthread_getspecific(g_key);
     *p += g_add;
   }
-  clock_gettime(CLOCK_MONOTONIC, &end);
-  printf("tls_pthread: %d\n", (int)shared::timespec_sub_us(end, start));
+  auto end = std::chrono::steady_clock::now();
+  std::chrono::duration<double> diff = end - start;
+  printf("tls_pthread: %fs\n", diff.count());
   pthread_key_delete(g_key);
 }
 
